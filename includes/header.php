0484f5cc60b5c81a32f0facd51dbc34566b2c828
mo-martin <momartin1@hotmail.co.uk>
2015-10-04 16:16:28 +0100

    Step 1 - Initial Nav

---
+++ includes/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en-gb">
+    <head>
+        <meta charset="utf-8">
+        <title><?php echo $title ?></title>
+		<meta name="description" content="Davis Brook Ltd Student Housing Manchester, Fallowfield, Lenshulme, Rusholme, University area">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="keywords" content="Nick, Brook, Davis, student, house, agency, housing, manchester, fallowfield, levenshulme, cheap, nice, kitchen, tv, desk, bed, lounge, bedroom">
+		<link href="images/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+		<link rel="stylesheet" href="css/styles.css">
+		<script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+		
+</head>
+
+
+<body id="top-page">
+
+<div id="skiplinks">
+			<p>
+				<a href="#skiptonav">Skip to navigation</a>!
+				<a href="#skipnav">Skip to Content</a>!
+			</p>
+</div>
+
+<nav class="mainnav">
+	<button class="navtoggle">toggle</button>
+		<ul class="links">
+			<li><a href="about.php">About Us</a></li>
+			<li><a href="properties.php">Our Properties</a></li>
+			<li><a href="contact.php">Contact</a></li>
+		</ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php $title = "Davis Brook Ltd Student Houses in Manchester, Rusholme, University Area, Levenshulme";
+include("includes/header.php"); ?>
+
+
+
+</body>
+</html>
